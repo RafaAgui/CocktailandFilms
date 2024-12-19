d7a1166ada443f81546b8dccf48fb678987c6802
RafaAgui <aguilaralvear@yahoo.es>
2024-12-19 12:20:23 +0100

    fix for eslint

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,14 @@
-async function getCocktails(): Promise<any> {
+async function getCocktails(): Promise<Cocktail[]> {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a');
-  const data = await response.json();
+  const data: { drinks: Cocktail[] } = await response.json();
   return data.drinks;
+}
+
+interface Cocktail {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+  strCategory: string;
 }
 
 export default getCocktails;
