08b0f01e3ae424dfb9fa5a4c451b354a8229e412
RafaAgui <aguilaralvear@yahoo.es>
2024-12-19 12:54:38 +0100

    update test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -5,3 +5,19 @@ describe('suite name', () => {
     expect(1).toBe(1);
   })
 })
+
+
+
+describe('example test', () => {
+  it('should handle promise rejection', async () => {
+    try {
+      await someAsyncFunction();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
+});
+
+function someAsyncFunction() {
+  throw new Error('Function not implemented.');
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,39 @@
-/// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
 
-export default getViteConfig({
+interface UserConfig {
+  // other properties
+  
+  test?: {
+    globals: boolean;
+    environment: string;
+    include: string[];
+    exclude: string[];
+    setupFiles: string;
+  };
+  
+  // other properties
+}
+
+// export default getViteConfig( <UserConfig>{
+// 	test: {
+// 		globals: true,
+//     environment: 'jsdom',
+//     include: ['./test/**/*.test.{js,ts,tsx}', './src/**/*.test.{js,ts,tsx}'],
+//     exclude: ['node_modules', 'dist'],
+// 		setupFiles: './setupTests.ts',
+// 	},
+// });
+
+
+const config: UserConfig = {
 	test: {
 		globals: true,
-    environment: 'jsdom',
-    include: ['./test/**/*.test.{js,ts,tsx}', './src/**/*.test.{js,ts,tsx}'],
-    exclude: ['node_modules', 'dist'],
-		setupFiles: './setupTests.ts',
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		exclude: ['node_modules', 'dist'],
+		setupFiles: './vitest.setup.ts',
 	},
-});
+};
+
+export default getViteConfig(config);
+
